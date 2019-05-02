1abc99cdf036dabd517bbceba133f1857af8371d
hgunther <hnsgunther@gmail.com>
2019-05-01 22:18:33 -0600

    Removed a comment

--- TutorialBot/globals.js
+++ TutorialBot/globals.js
@@ -1,12 +1,3 @@
-/*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('globals');
- * mod.thing == 'a thing'; // true
- */
-
 var globals = {
     spawnName: 'Spawn1'
 };
