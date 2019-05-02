ae0c457409d7364066021cd51a9412d4b29edba6
hgunther <hnsgunther@gmail.com>
2019-05-01 22:22:03 -0600

    Removed a section of code that was only useful for the tutorial

--- TutorialBot/main.js
+++ TutorialBot/main.js
@@ -7,21 +7,6 @@ module.exports.loop = function () {
     
     managerCreepCount.manage();
 
-    var tower = Game.getObjectById('e836a88c2c3b95049ad87973');
-    if(tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
-        });
-        if(closestDamagedStructure) {
-            tower.repair(closestDamagedStructure);
-        }
-
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if(closestHostile) {
-            tower.attack(closestHostile);
-        }
-    }
-
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
