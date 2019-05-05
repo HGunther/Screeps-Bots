d1c514962a772ffde9b6a839bb71c349c931d534
hgunther <hnsgunther@gmail.com>
2019-05-05 11:58:21 -0600

    Repair action now repairs nearby objects if available

--- TutorialBot/action.repair.js
+++ TutorialBot/action.repair.js
@@ -3,35 +3,36 @@ var actionRepair = {
     /** @param {Creep} creep **/
     repair: function (creep) {
         if (creep.carry.energy > 0) {
-            var targets = creep.room.find(FIND_MY_STRUCTURES, {
+            var repairable = creep.room.find(FIND_MY_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.hits < structure.hitsMax);
                 }
             });
-            // Try to repair the closest
-            if (targets.length < 1) {
+            // If none of my buildings need repair, check walls and roads
+            if (repairable.length < 1) {
                 // Finds roads and walls
-                targets = creep.room.find(FIND_STRUCTURES, {
+                repairable = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.hits < structure.hitsMax);
                     }
                 });
-                if (targets.length < 1) {
+                if (repairable.length < 1) {
                     // nothing to repair
                     return false;
                 }
             }
-            // Repair the lowest health
-            targets.sort((a, b) => a.hits - b.hits);
-            var structureToRepair = targets[0];
-            var result = creep.repair(structureToRepair);
+            // Repair what you're next to
+            var closest = creep.pos.findClosestByPath(repairable);
+            var result = creep.repair(closest);
             if (result == OK) {
                 // success!
                 return true;
             }
             if (result == ERR_NOT_IN_RANGE) {
-                // move closer
-                result = creep.moveTo(structureToRepair, {
+                // Move towards lowest health
+                repairable.sort((a, b) => a.hits - b.hits);
+                var lowestHealth = repairable[0];
+                result = creep.moveTo(lowestHealth, {
                     visualizePathStyle: {
                         stroke: '#ffffff'
                     }
