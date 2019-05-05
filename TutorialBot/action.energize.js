f1b4d2310f8c14948c875e24836c5209f9b504c4
hgunther <hnsgunther@gmail.com>
2019-05-05 11:45:24 -0600

    Fixed a bug where structures with zero energy could not be energized

--- TutorialBot/action.energize.js
+++ TutorialBot/action.energize.js
@@ -6,7 +6,7 @@ var actionEnergize = {
             // List energy structures
             var targets = creep.room.find(FIND_MY_STRUCTURES, {
                 filter: (structure) => {
-                    return (structure.energy && structure.energy < structure.energyCapacity);
+                    return (typeof(structure.energy) !== "undefined" && structure.energy < structure.energyCapacity);
                 }
             });
             // Try to build the closest
